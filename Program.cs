f8f208922e6369b8797838c7a60f29c142d8495b
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-11 14:53:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.calculate_age_using_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("what year were you born?");
+            int birthYear = int.Parse(Console.ReadLine());
+
+
+            int age = 2023 - birthYear;
+
+            Console.WriteLine(" Hi" + " you are " + name + " and you are " + age + " years old.");
+            Console.Read();
+
+             
+
+        }
+    }
+}
